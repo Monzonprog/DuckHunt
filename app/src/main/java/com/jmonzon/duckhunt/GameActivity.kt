54a5c2df79545c58bd4cfdadce2785d58ba159f8
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-07-01 22:46:59 +0200

    Creamos archivo de Constantes

--- app/src/main/java/com/jmonzon/duckhunt/GameActivity.kt
+++ app/src/main/java/com/jmonzon/duckhunt/GameActivity.kt
@@ -2,6 +2,7 @@ package com.jmonzon.duckhunt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jmonzon.duckhunt.common.Constantes
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/jmonzon/duckhunt/common/Constantes.kt
@@ -0,0 +1,7 @@
+package com.jmonzon.duckhunt.common
+
+class Constantes {
+    companion object{
+        var EXTRA_NICK: String = "nick"
+    }
+}
