2ee9ad833ddebb1a690a467f464f4e6cd44fc156
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-07-01 22:46:32 +0200

    Creamos intent para ir a nueva pantalla y arrastramos nick

--- app/src/main/java/com/jmonzon/duckhunt/GameActivity.kt
+++ app/src/main/java/com/jmonzon/duckhunt/GameActivity.kt
@@ -7,5 +7,7 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        val bundle: Bundle? = intent.extras
+        val nick: String? = bundle?.getString(Constantes.EXTRA_NICK, "Default")
     }
 }
--- app/src/main/java/com/jmonzon/duckhunt/LoginActivity.kt
+++ app/src/main/java/com/jmonzon/duckhunt/LoginActivity.kt
@@ -1,11 +1,29 @@
 package com.jmonzon.duckhunt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jmonzon.duckhunt.common.Constantes
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var nick: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        buttonStart.setOnClickListener {
+            nick = editTextNick.text.toString()
+
+            if (nick.isEmpty()){
+                editTextNick.error = getString(R.string.emptyNick)
+            } else{
+                val intent = Intent(this, GameActivity::class.java)
+                intent.putExtra(Constantes.EXTRA_NICK, nick)
+                startActivity(intent)
+            }
+        }
     }
 }
