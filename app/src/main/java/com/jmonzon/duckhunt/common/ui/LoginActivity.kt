84674acc0449ce14ea4861e28ac361a2ef2ebd20
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-07-02 22:33:28 +0200

    Refactor

--- app/src/main/java/com/jmonzon/duckhunt/common/ui/LoginActivity.kt
+++ app/src/main/java/com/jmonzon/duckhunt/common/ui/LoginActivity.kt
@@ -1,8 +1,11 @@
-package com.jmonzon.duckhunt
+package com.jmonzon.duckhunt.common.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.jmonzon.duckhunt.R
 import com.jmonzon.duckhunt.common.Constantes
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -14,16 +17,27 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        changeFont()
+
         buttonStart.setOnClickListener {
             nick = editTextNick.text.toString()
 
-            if (nick.isEmpty()){
+            if (nick.isEmpty()) {
                 editTextNick.error = getString(R.string.emptyNick)
-            } else{
+                Log.i("LoginActivity: ", "Nick en blanco")
+            } else {
                 val intent = Intent(this, GameActivity::class.java)
                 intent.putExtra(Constantes.EXTRA_NICK, nick)
                 startActivity(intent)
+                Log.i("LoginActivity: ", "Pasamos a pantalla de juego")
             }
         }
+    }
+
+    //Change font type
+    private fun changeFont(){
+        val typeface: Typeface = Typeface.createFromAsset(this.assets, "fonts/pixel.ttf")
+        editTextNick.typeface = typeface
+        buttonStart.typeface = typeface
     }
 }
