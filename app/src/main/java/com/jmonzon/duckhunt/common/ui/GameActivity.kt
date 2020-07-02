2c1188f21d4907e8faf9ee7e708ca8a773e3fa11
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-07-02 22:55:11 +0200

    Creamos Dialog de game over

--- app/src/main/java/com/jmonzon/duckhunt/common/ui/GameActivity.kt
+++ app/src/main/java/com/jmonzon/duckhunt/common/ui/GameActivity.kt
@@ -1,12 +1,15 @@
 package com.jmonzon.duckhunt.common.ui
 
 import android.content.Context
+import android.content.DialogInterface
 import android.graphics.Point
 import android.graphics.Typeface
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
 import android.view.Display
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.jmonzon.duckhunt.R
 import com.jmonzon.duckhunt.common.Constantes
@@ -20,6 +23,7 @@ class GameActivity : AppCompatActivity() {
     private var heightScreen = 0
     private lateinit var random: Random
     lateinit var context: Context
+    private var gameOver: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,8 +51,8 @@ class GameActivity : AppCompatActivity() {
         textViewNick.typeface = typeface
     }
 
+    //Get screen size
     private fun initDisplay() {
-        //Get screen size
         val display: Display = windowManager.defaultDisplay
         val size = Point()
         display.getSize(size)
@@ -61,29 +65,31 @@ class GameActivity : AppCompatActivity() {
 
     private fun eventsClickDuck() {
         imageViewDuck.setOnClickListener {
-            //Add 1 to counter and paint it when user hit the duck
-            counter++
-            textViewCounter.text = counter.toString()
-            imageViewDuck.setImageResource(R.drawable.duck_clicked)
+            if (!gameOver) {
+                //Add 1 to counter and paint it when user hit the duck
+                counter++
+                textViewCounter.text = counter.toString()
+                imageViewDuck.setImageResource(R.drawable.duck_clicked)
 
-            //Change image after time
-            Handler().postDelayed({
-                imageViewDuck.setImageResource(R.drawable.duck)
-                moveDuck()
-            }, 250L)
+                //Change image after time
+                Handler().postDelayed({
+                    imageViewDuck.setImageResource(R.drawable.duck)
+                    moveDuck()
+                }, 250L)
+            }
         }
 
     }
 
     private fun moveDuck() {
         val min = 0
-        //Prevent the duck from drawing off the screen
+        //Prevent draw the duck out of  screen
         val maxX: Int = widthScreen - imageViewDuck.width
         val maxY: Int = heightScreen - imageViewDuck.height
 
         //Generate two random number, one for X and another for Y
-        var randomX = random.nextInt((maxX - min) + 1)
-        var randomY = random.nextInt((maxY - min) + 1)
+        val randomX = random.nextInt((maxX - min) + 1)
+        val randomY = random.nextInt((maxY - min) + 1)
 
         //Use random number for move the duck to that point
         imageViewDuck.x = randomX.toFloat()
@@ -91,17 +97,46 @@ class GameActivity : AppCompatActivity() {
 
     }
 
-    //Method for count time from 60s to 0s
+    //Method for count time since 60s to 0s
     private fun initCountDown() {
-        object : CountDownTimer(60000, 1000) {
+        object : CountDownTimer(6000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                val textToShow: String = (millisUntilFinished / 1000).toString() +"s"
+                val textToShow: String = (millisUntilFinished / 1000).toString() + "s"
                 textViewTimer.text = textToShow
             }
 
             override fun onFinish() {
+                imageViewDuck.visibility = View.INVISIBLE
                 textViewTimer.text = "0s"
+                gameOver = true
+                showDialogGameOver()
             }
         }.start()
+    }
+
+    //Show dialog when game is finish
+    private fun showDialogGameOver() {
+        val builder: AlertDialog.Builder = this.let {
+            AlertDialog.Builder(it)
+        }
+
+        builder.apply {
+            setPositiveButton(R.string.restart
+            ) { dialog, id ->
+                // User clicked OK button
+            }
+            setNegativeButton(
+                R.string.exit
+            ) { dialog, id ->
+                dialog.dismiss()
+            }
+        }
+
+        val dialogText: String = resources.getString(R.string.duck_hunted, counter)
+        builder.setMessage(dialogText)
+            .setTitle(R.string.game_over)
+
+        val dialog: AlertDialog? = builder.create()
+        dialog!!.show()
     }
 }
