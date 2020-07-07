297f7232b3e3e07591f0987ebf1c63e4d782b6cc
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-07-07 20:25:24 +0200

    Refactor de variables ya que no coincidian con la BBDD

--- app/src/main/java/com/jmonzon/duckhunt/common/models/UserModel.kt
+++ app/src/main/java/com/jmonzon/duckhunt/common/models/UserModel.kt
@@ -1,5 +1,5 @@
 package com.jmonzon.duckhunt.common.models
 
-data class UserModel(var nick: String, var duck: Int) {
+data class UserModel(var nick: String, var ducks: Int) {
     constructor() : this("", 0)
 }
